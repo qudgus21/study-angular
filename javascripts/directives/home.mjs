dd35519434538e75a153b1f89445a13f8de29b03
황병현 <hwangbyeonghyeon@hwangbyeonghyeon-ui-MacBookPro.local>
2022-02-13 11:06:55 +0900

    feat: header 적용

--- javascripts/directives/home.mjs
+++ javascripts/directives/home.mjs
@@ -1,6 +1,5 @@
 //페이지를 구성하는 디렉티브(컴포넌트)
-
-angular.module('app').directive('calendar', function(){
+angular.module('app').directive('homeHeader', function(){
     return {
         templateUrl: '../../views/components/home/header.html',
     }
