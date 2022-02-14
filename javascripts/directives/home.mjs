0e2964109d061561ac6df8c54589fe74e67f622d
황병현 <hwangbyeonghyeon@hwangbyeonghyeon-ui-MacBookPro.local>
2022-02-14 13:54:50 +0900

    feat: directive 연결

--- javascripts/directives/home.mjs
+++ javascripts/directives/home.mjs
@@ -1,6 +1,25 @@
 //페이지를 구성하는 디렉티브(컴포넌트)
+
 angular.module('app').directive('homeHeader', function(){
     return {
         templateUrl: '../../views/components/home/header.html',
+    }
+})
+
+angular.module('app').directive('homeCalendar', function(){
+    return {
+        templateUrl: '../../views/components/home/calendar.html',
+    }
+})
+
+angular.module('app').directive('homeModal', function(){
+    return {
+        templateUrl: '../../views/components/home/modal.html',
+    }
+})
+
+angular.module('app').directive('modalContent', function(){
+    return {
+        templateUrl: '../../views/components/home/modalContent.html',
     }
 })
